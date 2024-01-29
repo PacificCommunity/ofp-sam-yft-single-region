823ebfa120717783a2659efc7200961bd8f39a79
Arni Magnusson <thisisarni@gmail.com>
2024-01-30 07:43:46 +1300

    Run model

--- model/admb/schaefer/model.R
+++ model/admb/schaefer/model.R
@@ -1,9 +1,23 @@
 ## Run analysis, write model results
 
-## Before:
-## After:
+## Before: yft.ctl, yft.dat (data), 0.2014.zip (boot/software)
+## After:  pella.tpl, pella (model)
 
 library(TAF)
 
 mkdir("model")
 
+# Unzip ADMB source code into model folder
+unzip("boot/software/0.2014.zip",
+      files="biomass-0.2014/model/pella/pella.tpl", junkpaths=TRUE,
+      exdir="model")
+
+# Compile ADMB model
+system("cd model; admb pella; rm pella.cpp pella.htp pella.obj")
+
+# Copy data files
+cp("data/yft.ctl", "model")
+cp("data/yft.dat", "model")
+
+# Run model
+system("cd model; pella -ind yft.dat")
