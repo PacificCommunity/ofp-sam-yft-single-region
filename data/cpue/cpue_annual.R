5e438afd5302f4d9cf7add333792bc6b7d065bd8
Arni Magnusson <thisisarni@gmail.com>
2024-01-30 07:42:40 +1300

    Divide CPUE index by 1e7

--- data/cpue/cpue_annual.R
+++ data/cpue/cpue_annual.R
@@ -5,6 +5,6 @@ cpue <- read.taf("single.region.index.csv")
 cpue.annual <- aggregate(Index~floor(Time), cpue, mean)
 names(cpue.annual) <- c("Year", "CPUE")
 
-cpue.annual$CPUE <- round(cpue.annual$CPUE / 1e6, 1)
+cpue.annual$CPUE <- round(cpue.annual$CPUE / 1e7, 1)
 
 write.taf(cpue.annual)
