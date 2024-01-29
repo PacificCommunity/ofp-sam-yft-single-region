abc5d3e8b86ad00e793bd61927e61e4fb6f874a4
Arni Magnusson <thisisarni@gmail.com>
2024-01-30 07:58:27 +1300

    Plot biomass and index

--- model/admb/schaefer/report.R
+++ model/admb/schaefer/report.R
@@ -1,9 +1,25 @@
 ## Prepare plots and tables for report
 
-## Before:
-## After:
+## Before: summary.csv (output)
+## After:  biomass.png, index.png (report)
 
 library(TAF)
 
 mkdir("report")
 
+# Read summary table
+summary <- read.taf("output/summary.csv")
+
+# Plot biomass
+taf.png("biomass")
+z <- barplot(Catch~Year, summary, ylim=lim(summary$Biomass),
+             ylab="Biomass and catch")
+lines(z, summary$Biomass)
+box()
+dev.off()
+
+# Plot index
+taf.png("index")
+plot(Index~Year, summary, ylim=lim(c(summary$Index, summary$IndexFit)))
+lines(IndexFit~Year, summary)
+dev.off()
