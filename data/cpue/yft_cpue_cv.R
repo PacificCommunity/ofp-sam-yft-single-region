1ac5133add36ff1e00d9020d89141a129a1e21c3
Arni Magnusson <thisisarni@gmail.com>
2025-06-06 17:33:12 +1100

    Implement two-way sigma<->flag conversion

--- data/cpue/yft_cpue_cv.R
+++ data/cpue/yft_cpue_cv.R
@@ -1,4 +1,11 @@
-convert.flag <- function(flag.value)
+sigma2flag <- function(sigma)
+{
+  penalty <- 1 / (2*sigma^2)
+  flag.value <- 10 * penalty
+  flag.value
+}
+
+flag2sigma <- function(flag.value)
 {
   penalty <- flag.value / 10
   sigma <- sqrt(1/(2*penalty))
@@ -17,7 +24,7 @@ cpue.settings <- read.table(text=doitall, skip=skip, nrows=5)
 flag.value <- cpue.settings$V6
 
 # Convert to sigma
-sigma <- convert.flag(flag.value)
+sigma <- flag2sigma(flag.value)
 
 # Plot
 barplot(sigma, names=1:5, xlab="region", ylab="cv", ylim=c(0,0.3))
